7e4717189045b454668624adc855b5e8d4d9fa59
Jo <306894833+resoluterecordsjkp@users.noreply.github.com>
2026-07-19 21:19:35 -0500

    Clear service worker cache to force reload of all assets

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // LinchKey build: 2026-07-19 / Contest PWA v1.0.1
-const CACHE='linchkey-contest-v1-0-1';
+const CACHE='linchkey-contest-v1-0-2';
 const ASSETS=['./','index.html','styles.css','app.js','config.json','manifest.webmanifest','modules.json','linchkey-logo.jpg'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
